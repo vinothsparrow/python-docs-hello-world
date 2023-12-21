0427cf3212988dc205b92555f2fa73c7027378f9
Vinoth Kumar <3402812+vinothsparrow@users.noreply.github.com>
2023-12-21 22:51:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "PoC by Vinoth Kumar (<a href=https://hackerone.com/vinothkumar>https://hackerone.com/vinothkumar</a>)"
